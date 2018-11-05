64ef4ff4a3ae727e7bb34c2fefc9ef2761af535f
alfarhanzahedi <alfarhanzahedi@gmail.com>
2018-11-05 19:30:19 +0530

    added scroll on "explore" feature

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-var countDownDate = new Date("Jan 18, 2019 10:00:00").getTime();
-
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    days = days < 10 ? '0' + days : days;
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    document.getElementById("countdown-timer-days").textContent = days;
-    document.getElementById("countdown-timer-hours").textContent = hours;
-    document.getElementById("countdown-timer-minutes").textContent = minutes;
-    document.getElementById("countdown-timer-seconds").textContent = seconds;
-}, 1000);
+$(document).ready(function(){
+    $('#explore-button').on('click', function(){
+        console.log('Hi');
+        $('#landing-overlay').css({ 'top': '-100%' });
+        $('#main').css({ 'top': '0%' });
+    });
+});
