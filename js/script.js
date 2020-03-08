5a4797886b0713562d7a6afca76863709c819f29
Subhasis Das <subhasisdas125@gmail.com>
2020-03-08 17:18:25 +0530

    team added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,95 @@
+$(document).ready(function() {
+  countDownTimer();
+  $("#explore-button").on("click", function() {
+    $("html,body").animate(
+      {
+        scrollTop: $("#main").offset().top
+      },
+      "slow"
+    );
+  });
+
+  getTeam();
+});
+
+/** ===TEAM RENDERING JS=== */
+async function getTeam() {
+  const members = await $.getJSON("/js/team.json");
+  let outputMarkup = "";
+  members.forEach(row => {
+    outputMarkup += `<div class="row">`;
+    row.forEach(member => {
+      outputMarkup += `
+      <div class="col-sm-4 container-team-member">
+        <div class="float-my-children">
+          <img src="${member.picture}" alt="${member.name}'s picture" />
+          <div class="team-member-details">
+            <span class="team-member-name">${member.name}</span><br/>
+            <span>${member.designation}</span><br />
+            <span>
+              <div style="margin:0px auto">
+                <ul>
+                  ${getLinks(member)}
+                </ul>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    `;
+    });
+    outputMarkup += "</div>";
+  });
+
+  $("#container-team").html(outputMarkup);
+}
+
+function getLinks(member) {
+  let markup = "";
+  if (member.facebook) {
+    markup += `
+    <li>
+      <a href=  ${member.facebook} target="noopener noreferrer" aria-label="${member.name}' facebook account">
+        <i class="fab fa-facebook"></i>
+      </a>
+    </li>
+    `;
+  }
+
+  if (member.linkedin) {
+    markup += `
+    <li>
+      <a href=  ${member.linkedin} target="noopener noreferrer" aria-label="${member.name}' linkedin account">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+    </li>
+    `;
+  }
+
+  if (member.twitter) {
+    markup += `
+    <li>
+      <a href=  ${member.twitter} target="noopener noreferrer" aria-label="${member.name}' twitter account">
+        <i class="fab fa-twitter"></i>
+      </a>
+    </li>
+    `;
+  }
+
+  if (member.website) {
+    markup += `
+    <li>
+      <a href=  ${member.website} target="noopener noreferrer" aria-label="${member.name}' website">
+        <i class="fa fa-globe"></i>
+      </a>
+    </li>
+    `;
+  }
+
+  return markup;
+}
+
+/** COUNTDOWN */
 var eventDate = new Date("Mar 24, 2020 10:00:00").getTime();
 
 var countDownTimer = () =>
@@ -19,109 +111,3 @@ var countDownTimer = () =>
     document.getElementById("countdown-timer-minutes").textContent = minutes;
     document.getElementById("countdown-timer-seconds").textContent = seconds;
   }, 1000);
-
-$(document).ready(function() {
-  countDownTimer();
-  $("#explore-button").on("click", function() {
-    $("html,body").animate(
-      {
-        scrollTop: $("#main").offset().top
-      },
-      "slow"
-    );
-  });
-
-  /*getTeam();
- 
-  $("#show-extra-members").on("click", function() {
-    $("#container-team-extra").slideToggle("slow");
-    var element = $("#show-extra-members-content");
-    if (element.text() == "Show 'em all") element.text("Hide 'em all");
-    else element.text("Show 'em all");
-  });
-
-  function getLinks(member) {
-    var result = "";
-    var links = member.links;
-    links.forEach(function(link) {
-      if (link.linkType === "Twitter") {
-        result +=
-          '<li><a rel="noreferrer noopener" href="' +
-          link.link +
-          '" target="_blank" aria-label="' +
-          member.name +
-          '\'s Twitter account."><i class="fab fa-twitter"></i></a></li>';
-      }
-      if (link.linkType == "LinkedIn") {
-        result +=
-          '<li><a rel="noreferrer noopener" href="' +
-          link.link +
-          '" target="_blank" aria-label="' +
-          member.name +
-          '\'s LinkedIn account."><i class="fab fa-linkedin"></i></a></li>';
-      }
-      if (link.linkType == "Blog") {
-        result +=
-          '<li><a rel="noreferrer noopener" href="' +
-          link.link +
-          '" target="_blank" aria-label="' +
-          member.name +
-          '\'s blog."><i class="fas fa-link"></i></a></li>';
-      }
-      if (link.linkType == "Facebook") {
-        result +=
-          '<li><a rel="noreferrer noopener" href="' +
-          link.link +
-          '" target="_blank" aria-label="' +
-          member.name +
-          '\'s Facebook profile."><i class="fab fa-facebook-f"></i></a></li>';
-      }
-    });
-    return result;
-  }
-
-  function getTemplate(member) {
-    var template =
-      `<div class="col-lg-3 col-md-3 col-sm-12 container-team-member">
-                <div class=" float-my-children">
-                    <img src="` +
-      member.displayPicture +
-      `" alt="` +
-      member.name +
-      `'s portrait." />
-                    <div class="team-member-details">
-                        <span class="team-member-name">` +
-      member.name +
-      `</span>
-                        <br />
-                        <span>` +
-      member.description +
-      `</span>
-                        <span>
-                            <div style="margin: 0px auto;"><ul>` +
-      getLinks(member) +
-      `</ul></div>
-                        </span>
-                    </div>
-                </div>
-            </div>`;
-    return template;
-  }
-  var html = "";
-
-  function getTeam() {
-    $.getJSON("js/team.json", function(members) {
-      members.forEach(function(member) {
-        html += getTemplate(member);
-      });
-      $("#container-team").append(html);
-      html = "";
-      $.getJSON("js/extra-team.json", function(members) {
-        members.forEach(function(member) {
-          html += getTemplate(member);
-        });
-        $("#container-team-extra").append(html);
-      });
-    });
-  }*/
-});
